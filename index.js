ddd92e0bd2ba789f7f8dc28c727494c0783bfa93
Shalahuddin360 <shalahuddinislam792@gmail.com>
2023-02-18 08:00:32 +0600

    collected data from html using id for each card text element and two input fields

--- index.js
+++ index.js
@@ -1,28 +1,62 @@
 // for first card 
-document.getElementById('first-card').addEventListener('click', function () {
-//get the data from html using id
-    const  firstName = document.getElementById('first-name').innerText;
+let serial =0;
+document.getElementById('btn-first').addEventListener('click', function () {
+    serial += 1;
+    //get the data from html using id
+    const firstName = document.getElementById('first-name').innerText;
 
     const triangleFirstInputField = document.getElementById('triangle-firstInput-field');
     const triangleFirstInputFieldValueString = triangleFirstInputField.value;
     const triangleFirstInputFieldValue = parseFloat(triangleFirstInputFieldValueString);
-    triangleFirstInputField.value ='';
+    triangleFirstInputField.value = '';
     const triangleSecondInputField = document.getElementById('triangle-secondInput-field');
     const triangleSecondInputFieldValueString = triangleSecondInputField.value;
     const triangleSecondInputFieldValue = parseFloat(triangleSecondInputFieldValueString);
-    triangleSecondInputField.value ='';
-    const triangleAreaTotal =0.5 * triangleFirstInputFieldValue * triangleSecondInputFieldValue
-    const container =document.getElementById('table-container')
-    const tr = document.createElement('tr');
-    
-   
+    triangleSecondInputField.value = '';
 
-   
+    // multiply
+    const triangleAreaTotal = 0.5 * triangleFirstInputFieldValue * triangleSecondInputFieldValue;
+
+    //show the data
+    displayData(firstName, triangleAreaTotal)
+})
+
+// for second card 
+document.getElementById('btn-second').addEventListener('click', function () {
+    serial +=1;
+    //get the data from html using id
+    const secondName = document.getElementById('second-name').innerText;
+
+    const rectangleFirstInputField = document.getElementById('rectangle-firstInput-field');
+    const rectangleFirstInputFieldValueString = rectangleFirstInputField.value;
+    const rectangleFirstInputFieldValue = parseFloat(rectangleFirstInputFieldValueString);
+    rectangleFirstInputField.value = '';
+    const rectangleSecondInputField = document.getElementById('rectangle-secondInput-field');
+    const rectangleSecondInputFieldValueString = rectangleSecondInputField.value;
+    const rectangleSecondInputFieldValue = parseFloat(rectangleSecondInputFieldValueString);
+    rectangleSecondInputField.value = '';
+
+    // multiply
+    const rectangleAreaTotal = 0.5 * rectangleFirstInputFieldValue * rectangleSecondInputFieldValue;
+
+    //show the data
+    displayData(secondName, rectangleAreaTotal)
+})
+
+
+
+// common function to display data 
+
+function displayData(geometryOfName,geometryOfAreaTotal) {
+    const container = document.getElementById('table-container')
+    const tr = document.createElement('tr');
+
+
     tr.innerHTML = `
-      <td>${1}</td>
-      <td>${firstName}</td>
-      <td>${triangleAreaTotal}cm<sup>2<sup></td>
+      <td>${serial}</td>
+      <td>${geometryOfName}</td>
+      <td>${geometryOfAreaTotal}cm<sup>2<sup></td>
       <td><button>Convert To m<sup>2<sup> </button></td>
        `;
-       container.appendChild(tr);
-})
+    container.appendChild(tr);
+}
