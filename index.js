ff8427df8eae0f774185b816037a34ebc3147c92
Shalahuddin360 <shalahuddinislam792@gmail.com>
2023-02-18 11:08:39 +0600

    In the fifth commit are added , the common function is called using the name of each card and the id of the two input fields

--- index.js
+++ index.js
@@ -1,53 +1,171 @@
-// for first card 
-let serial =0;
+/*****************for first card ********************/
+let serial = 0;
 document.getElementById('btn-first').addEventListener('click', function () {
     serial += 1;
     //get the data from html using id
-    const firstName = document.getElementById('first-name').innerText;
 
-    const triangleFirstInputField = document.getElementById('triangle-firstInput-field');
-    const triangleFirstInputFieldValueString = triangleFirstInputField.value;
-    const triangleFirstInputFieldValue = parseFloat(triangleFirstInputFieldValueString);
-    triangleFirstInputField.value = '';
-    const triangleSecondInputField = document.getElementById('triangle-secondInput-field');
-    const triangleSecondInputFieldValueString = triangleSecondInputField.value;
-    const triangleSecondInputFieldValue = parseFloat(triangleSecondInputFieldValueString);
-    triangleSecondInputField.value = '';
+    const firstName = getTextElementById('first-name')
+    const triangleFirstInputFieldValue = getFirstInputFieldValueById('triangle-firstInput-field');
+    const triangleSecondInputFieldValue = getSecondInputFieldValueById('triangle-secondInput-field');
 
     // multiply
     const triangleAreaTotal = 0.5 * triangleFirstInputFieldValue * triangleSecondInputFieldValue;
 
     //show the data
-    displayData(firstName, triangleAreaTotal)
+    displayData(firstName, triangleAreaTotal);
+    //button disabled
+    disabledButton('btn-first');
 })
 
-// for second card 
+/**************************** for second card ***************************** */
 document.getElementById('btn-second').addEventListener('click', function () {
-    serial +=1;
+    serial += 1;
     //get the data from html using id
-    const secondName = document.getElementById('second-name').innerText;
 
-    const rectangleFirstInputField = document.getElementById('rectangle-firstInput-field');
-    const rectangleFirstInputFieldValueString = rectangleFirstInputField.value;
-    const rectangleFirstInputFieldValue = parseFloat(rectangleFirstInputFieldValueString);
-    rectangleFirstInputField.value = '';
-    const rectangleSecondInputField = document.getElementById('rectangle-secondInput-field');
-    const rectangleSecondInputFieldValueString = rectangleSecondInputField.value;
-    const rectangleSecondInputFieldValue = parseFloat(rectangleSecondInputFieldValueString);
-    rectangleSecondInputField.value = '';
-
+    const secondName = getTextElementById('second-name')
+    const rectangleFirstInputFieldValue = getFirstInputFieldValueById('rectangle-firstInput-field');
+    const rectangleSecondInputFieldValue = getSecondInputFieldValueById('rectangle-secondInput-field');
+    if (
+        rectangleFirstInputFieldValue == "" ||
+        rectangleSecondInputFieldValue == "" ||
+        rectangleFirstInputFieldValue <= 0 ||
+        rectangleSecondInputFieldValue <= 0
+    ) {
+        return alert("please enter any valid number");
+    }
     // multiply
-    const rectangleAreaTotal = 0.5 * rectangleFirstInputFieldValue * rectangleSecondInputFieldValue;
+    const rectangleAreaTotal = rectangleFirstInputFieldValue * rectangleSecondInputFieldValue;
 
     //show the data
-    displayData(secondName, rectangleAreaTotal)
+    displayData(secondName, rectangleAreaTotal);
+    //button disabled
+    disabledButton('btn-second');
 })
 
+
+/**************************** for third card ***************************** */
+document.getElementById('btn-third').addEventListener('click', function () {
+    serial += 1;
+    //get the data from html using id
+
+    const thirdName = getTextElementById('third-name')
+    const parallelogramFirstInputFieldValue = getFirstInputFieldValueById('parallelogram-firstInput-field');
+    const parallelogramSecondInputFieldValue = getSecondInputFieldValueById('parallelogram-secondInput-field');
+    if (
+        parallelogramFirstInputFieldValue == "" ||
+        parallelogramSecondInputFieldValue == "" ||
+        parallelogramFirstInputFieldValue <= 0 ||
+        parallelogramSecondInputFieldValue <= 0
+    ) {
+        return alert("please enter any valid number");
+    }
+    // multiply
+    const parallelogramAreaTotal = parallelogramFirstInputFieldValue * parallelogramSecondInputFieldValue;
+
+    //show the data
+    displayData(thirdName, parallelogramAreaTotal);
+    //button disabled
+    disabledButton('btn-third');
+})
+/**************************** for fourth card ***************************** */
+document.getElementById('btn-fourth').addEventListener('click', function () {
+    serial += 1;
+    //get the data from html using id
+
+    const fourthName = getTextElementById('fourth-name')
+    const rhombusFirstInputFieldValue = getFirstInputFieldValueById('rhombus-firstInput-field');
+    const rhombusSecondInputFieldValue = getSecondInputFieldValueById('rhombus-secondInput-field');
+    if (
+        rhombusFirstInputFieldValue == "" ||
+        rhombusSecondInputFieldValue == "" ||
+        rhombusFirstInputFieldValue <= 0 ||
+        rhombusSecondInputFieldValue <= 0
+    ) {
+        return alert("please enter any valid number");
+    }
+    // multiply
+    const rhombusAreaTotal = 0.5 * rhombusFirstInputFieldValue * rhombusSecondInputFieldValue;
+
+    //show the data
+    displayData(fourthName, rhombusAreaTotal);
+    //button disabled
+    disabledButton('btn-fourth');
+})
+/**************************** for fifth card ***************************** */
+document.getElementById('btn-fifth').addEventListener('click', function () {
+    serial += 1;
+    //get the data from html using id
+
+    const fifthName = getTextElementById('fifth-name')
+    const pentagonFirstInputFieldValue = getFirstInputFieldValueById('pentagon-firstInput-field');
+    const pentagonSecondInputFieldValue = getSecondInputFieldValueById('pentagon-secondInput-field');
+    if (
+        pentagonFirstInputFieldValue == "" ||
+        pentagonSecondInputFieldValue == "" ||
+        pentagonFirstInputFieldValue <= 0 ||
+        pentagonSecondInputFieldValue <= 0
+    ) {
+        return alert("please enter any valid number");
+    }
+    // multiply
+    const pentagonAreaTotal =0.5 * pentagonFirstInputFieldValue * pentagonSecondInputFieldValue;
+
+    //show the data
+    displayData(fifthName, pentagonAreaTotal);
+    //button disabled
+    disabledButton('btn-fifth');
+})
+/**************************** for sixth card ***************************** */
+document.getElementById('btn-sixth').addEventListener('click', function () {
+    serial += 1;
+    //get the data from html using id
+
+    const sixthName = getTextElementById('sixth-name')
+    const ellipseFirstInputFieldValue = getFirstInputFieldValueById('ellipse-firstInput-field');
+    const ellipseSecondInputFieldValue = getSecondInputFieldValueById('ellipse-secondInput-field');
+    if (
+        ellipseFirstInputFieldValue == "" ||
+        ellipseSecondInputFieldValue == "" ||
+        ellipseFirstInputFieldValue <= 0 ||
+        ellipseSecondInputFieldValue <= 0
+    ) {
+        return alert("please enter any valid number");
+    }
+    // multiply
+    const ellipseAreaTotal =0.5 * ellipseFirstInputFieldValue * ellipseSecondInputFieldValue;
+
+    //show the data
+    displayData(sixthName, ellipseAreaTotal);
+    //button disabled
+    disabledButton('btn-sixth');
+})
 
 
 // common function to display data 
 
-function displayData(geometryOfName,geometryOfAreaTotal) {
+function getTextElementById(elementId) {
+    const geometryOfName = document.getElementById(elementId).innerText;
+    return geometryOfName;
+}
+function getFirstInputFieldValueById(inputFieldFirstId) {
+
+
+    const geometryOfFirstInputField = document.getElementById(inputFieldFirstId);
+    const geometryOfFirstInputFieldValueString = geometryOfFirstInputField.value;
+    const geometryOfFirstInputFieldValue = parseFloat(geometryOfFirstInputFieldValueString);
+    geometryOfFirstInputField.value = '';
+    return geometryOfFirstInputFieldValue;
+
+}
+function getSecondInputFieldValueById(inputFieldSecondId) {
+    const geometryOfSecondInputField = document.getElementById(inputFieldSecondId);
+    const geometryOfSecondInputFieldValueString = geometryOfSecondInputField.value;
+    const geometryOfSecondInputFieldValue = parseFloat(geometryOfSecondInputFieldValueString);
+    geometryOfSecondInputField.value = '';
+    return geometryOfSecondInputFieldValue;
+}
+
+function displayData(geometryOfName, geometryOfAreaTotal) {
     const container = document.getElementById('table-container')
     const tr = document.createElement('tr');
 
@@ -56,7 +174,12 @@ function displayData(geometryOfName,geometryOfAreaTotal) {
       <td>${serial}</td>
       <td>${geometryOfName}</td>
       <td>${geometryOfAreaTotal}cm<sup>2<sup></td>
-      <td><button>Convert To m<sup>2<sup> </button></td>
+      <td><button class="btn btn-sm bg-blue-400"><small>Convert To</small><small>m</small><sup>2<sup> </button></td>
+    
        `;
     container.appendChild(tr);
+}
+//   button disabled common function 
+function disabledButton(id) {
+    document.getElementById(id).setAttribute('disabled', true);
 }
