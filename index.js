dc229349763891acf848082adc9f59cd344eab6b
Shalahuddin360 <shalahuddinislam792@gmail.com>
2023-02-18 19:28:06 +0600

    In the 8th commit all card input field numbers , string , boolean , type and array , negative value character blank input fields have beeen validated

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ document.getElementById('btn-first').addEventListener('click', function () {
     const firstName = getTextElementById('first-name')
     const triangleFirstInputFieldValue = getFirstInputFieldValueById('triangle-firstInput-field');
     const triangleSecondInputFieldValue = getSecondInputFieldValueById('triangle-secondInput-field');
+    // validation for first card 
 
+    if (isNaN(triangleFirstInputFieldValue) || triangleFirstInputFieldValue === '' || triangleFirstInputFieldValue <= 0 || triangleFirstInputFieldValue === String || isNaN(triangleSecondInputFieldValue) || triangleSecondInputFieldValue === '' || triangleSecondInputFieldValue <= 0 || triangleSecondInputFieldValue === String) {
+        return alert('please provide a valid number')
+    }
     // multiply
     const triangleAreaTotal = (0.5 * triangleFirstInputFieldValue * triangleSecondInputFieldValue).toFixed(2);
 
@@ -25,11 +29,11 @@ document.getElementById('btn-second').addEventListener('click', function () {
     const secondName = getTextElementById('second-name')
     const rectangleFirstInputFieldValue = getFirstInputFieldValueById('rectangle-firstInput-field');
     const rectangleSecondInputFieldValue = getSecondInputFieldValueById('rectangle-secondInput-field');
-    if ((rectangleFirstInputFieldValue == "" || rectangleSecondInputFieldValue == "" ||
-        rectangleFirstInputFieldValue <= 0 ||
-        rectangleSecondInputFieldValue <= 0)
-    ) {
-        return alert("please enter any valid number");
+
+    //   validation for second card 
+
+    if (isNaN(rectangleFirstInputFieldValue) || rectangleFirstInputFieldValue === '' || rectangleFirstInputFieldValue <= 0 || rectangleFirstInputFieldValue === String || isNaN(rectangleSecondInputFieldValue) || rectangleSecondInputFieldValue === '' || rectangleSecondInputFieldValue <= 0 || rectangleSecondInputFieldValue === String) {
+        return alert('please provide a valid number')
     }
     // multiply
     const rectangleAreaTotal = (rectangleFirstInputFieldValue * rectangleSecondInputFieldValue).toFixed(2);
@@ -49,13 +53,10 @@ document.getElementById('btn-third').addEventListener('click', function () {
     const thirdName = getTextElementById('third-name')
     const parallelogramFirstInputFieldValue = getFirstInputFieldValueById('parallelogram-firstInput-field');
     const parallelogramSecondInputFieldValue = getSecondInputFieldValueById('parallelogram-secondInput-field');
-    if (
-        parallelogramFirstInputFieldValue == "" ||
-        parallelogramSecondInputFieldValue == "" ||
-        parallelogramFirstInputFieldValue <= 0 ||
-        parallelogramSecondInputFieldValue <= 0
-    ) {
-        return alert("please enter any valid number");
+
+    //  validation for third-card 
+    if (isNaN(parallelogramFirstInputFieldValue) || parallelogramFirstInputFieldValue === '' || parallelogramFirstInputFieldValue <= 0 || parallelogramFirstInputFieldValue === String || isNaN(parallelogramSecondInputFieldValue) || parallelogramSecondInputFieldValue === '' || parallelogramSecondInputFieldValue <= 0 || parallelogramSecondInputFieldValue === String) {
+        return alert('please provide a valid number')
     }
     // multiply
     const parallelogramAreaTotal = (parallelogramFirstInputFieldValue * parallelogramSecondInputFieldValue).toFixed(2);
@@ -73,13 +74,11 @@ document.getElementById('btn-fourth').addEventListener('click', function () {
     const fourthName = getTextElementById('fourth-name')
     const rhombusFirstInputFieldValue = getFirstInputFieldValueById('rhombus-firstInput-field');
     const rhombusSecondInputFieldValue = getSecondInputFieldValueById('rhombus-secondInput-field');
-    if (
-        rhombusFirstInputFieldValue == "" ||
-        rhombusSecondInputFieldValue == "" ||
-        rhombusFirstInputFieldValue <= 0 ||
-        rhombusSecondInputFieldValue <= 0
-    ) {
-        return alert("please enter any valid number");
+
+    //  validation for fourth-card 
+    if (isNaN(rhombusFirstInputFieldValue) || rhombusFirstInputFieldValue === '' || rhombusFirstInputFieldValue <= 0 || rhombusFirstInputFieldValue === String || isNaN(rhombusSecondInputFieldValue) || rhombusSecondInputFieldValue === '' || rhombusSecondInputFieldValue <= 0 || rhombusSecondInputFieldValue === String) {
+        return alert('please provide a valid number');
+
     }
     // multiply
     const rhombusAreaTotal = (0.5 * rhombusFirstInputFieldValue * rhombusSecondInputFieldValue).toFixed(2);
@@ -97,13 +96,9 @@ document.getElementById('btn-fifth').addEventListener('click', function () {
     const fifthName = getTextElementById('fifth-name')
     const pentagonFirstInputFieldValue = getFirstInputFieldValueById('pentagon-firstInput-field');
     const pentagonSecondInputFieldValue = getSecondInputFieldValueById('pentagon-secondInput-field');
-    if (
-        pentagonFirstInputFieldValue == "" ||
-        pentagonSecondInputFieldValue == "" ||
-        pentagonFirstInputFieldValue <= 0 ||
-        pentagonSecondInputFieldValue <= 0
-    ) {
-        return alert("please enter any valid number");
+    //  validation for fifth-card 
+    if (isNaN(pentagonFirstInputFieldValue) || pentagonFirstInputFieldValue === '' || pentagonFirstInputFieldValue <= 0 || pentagonFirstInputFieldValue === String || isNaN(pentagonSecondInputFieldValue) || pentagonSecondInputFieldValue === '' || pentagonSecondInputFieldValue <= 0 || pentagonSecondInputFieldValue === String) {
+        return alert('please provide a valid number')
     }
     // multiply
     const pentagonAreaTotal = (0.5 * pentagonFirstInputFieldValue * pentagonSecondInputFieldValue).toFixed(2);
@@ -121,13 +116,9 @@ document.getElementById('btn-sixth').addEventListener('click', function () {
     const sixthName = getTextElementById('sixth-name')
     const ellipseFirstInputFieldValue = getFirstInputFieldValueById('ellipse-firstInput-field');
     const ellipseSecondInputFieldValue = getSecondInputFieldValueById('ellipse-secondInput-field');
-    if (
-        ellipseFirstInputFieldValue == "" ||
-        ellipseSecondInputFieldValue == "" ||
-        ellipseFirstInputFieldValue <= 0 ||
-        ellipseSecondInputFieldValue <= 0
-    ) {
-        return alert("please enter any valid number");
+    //  validation for sixth-card 
+    if (isNaN(ellipseFirstInputFieldValue) || ellipseFirstInputFieldValue === '' || ellipseFirstInputFieldValue <= 0 || ellipseFirstInputFieldValue === String || isNaN(ellipseSecondInputFieldValue) || ellipseSecondInputFieldValue === '' || ellipseSecondInputFieldValue <= 0 || ellipseSecondInputFieldValue === String) {
+        return alert('please provide a valid number')
     }
     // multiply
     const ellipseAreaTotal = (3.14 * ellipseFirstInputFieldValue * ellipseSecondInputFieldValue).toFixed(2);
